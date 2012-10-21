2c098af5a33ca28b5f42b62ab103453843f43336
arigatuo <arigatuo@gmail.com>
2012-10-22 01:11:04 +0800

    logochanges

--- protected/views/main/index.php
+++ protected/views/main/index.php
@@ -3,40 +3,80 @@
         <h1>What's meaning of </h1>
         <h1>SMZLX</h1>
     </div>
-    <div class="title" id="about_chisono">
-        <h3>CHI SONO</h3>
-        <img alt="Web Tailoring" src="<?php echo Yii::app()->request->baseUrl?>/images/tailoring_high.jpg">
-        <div class="clear"></div>
-    </div>
-    <div class="about_info">
-        <p class="about_info_par01">Sono Danilo Giagnoli, un grafico web designer con più di 8 anni di esperienza.</p>
-        <p class="about_info_par02"> Mi occupo della progettazione e della realizzazione di siti web curando i progetti in ogni minimo dettaglio:
-            architettura informativa, wireframe, user experience, concept grafico e implementazione (xhtml/css).<br>
-            <br>
-            Tra le varie esperienze maturate, in agenzia o come freelance, ho avuto la possibilità di confrontarmi con molteplici problematiche
-            e tipologie di lavori, che mi hanno permesso di spaziare ed esplorare diversi stili, rendendomi un creativo dinamico e versatile
-            in grado di realizzare un "vestito fatto su misura".<br>
-            <br>
-            Offro servizi legati al web (siti internet, personalizzazione cms, conversioni da psd a html, banner pubblicitari) e alla grafica (loghi, immagine coordinata, brochure, cataloghi, flyer, inviti, illustrazioni).<br>
-            <br>
-            Se siete interessati ad avere una mia consulenza o collaborazione, non esitate a contattarmi. </p>
-        <div class="about_info_cvMail">
-            <p class="about_info_cv"><img alt="cv icon" src="images/cv_image.jpg"><a target="_blank" href="cv/cv_danilogiagnoli.pdf">SCARICA IL C.V.</a></p>
-            <p class="about_info_mail">info@danilogiagnoli.com</p>
+    <div class="article">
+        <div class="title" id="about_chisono">
+            <h3>CHI SONO</h3>
+            <img alt="Web Tailoring" src="<?php echo Yii::app()->request->baseUrl?>/images/tailoring_high.jpg">
+            <div class="clear"></div>
+        </div>
+        <div class="about_info">
+            <p class="about_info_par01">Sono Danilo Giagnoli, un grafico web designer con più di 8 anni di esperienza.</p>
+            <p class="about_info_par02"> Mi occupo della progettazione e della realizzazione di siti web curando i progetti in ogni minimo dettaglio:
+                architettura informativa, wireframe, user experience, concept grafico e implementazione (xhtml/css).<br>
+                <br>
+                Tra le varie esperienze maturate, in agenzia o come freelance, ho avuto la possibilità di confrontarmi con molteplici problematiche
+                e tipologie di lavori, che mi hanno permesso di spaziare ed esplorare diversi stili, rendendomi un creativo dinamico e versatile
+                in grado di realizzare un "vestito fatto su misura".<br>
+                <br>
+                Offro servizi legati al web (siti internet, personalizzazione cms, conversioni da psd a html, banner pubblicitari) e alla grafica (loghi, immagine coordinata, brochure, cataloghi, flyer, inviti, illustrazioni).<br>
+                <br>
+                Se siete interessati ad avere una mia consulenza o collaborazione, non esitate a contattarmi. </p>
+            <div class="about_info_cvMail">
+                <p class="about_info_cv"><img alt="cv icon" src="images/cv_image.jpg"><a target="_blank" href="cv/cv_danilogiagnoli.pdf">SCARICA IL C.V.</a></p>
+                <p class="about_info_mail">info@danilogiagnoli.com</p>
+            </div>
+        </div>
+        <div class="project_info">
+            <div class="project_info_text">
+                <h4>Grafica</h4>
+                <p>Ideazione e realizzazione di un poster promozionale.</p>
+            </div>
+            <img alt="Web Tailoring" src="images/ago.jpg">
+            <ul class="project_info_text2">
+                <li><b>Cliente:</b> Personale</li>
+                <li><b>Anno:</b> 2009</li>
+                <li><b>Strumenti:</b> photoshop, illustrator</li>
+                <li class="btn_project"></li>
+            </ul>
+            <div class="clear"></div>
         </div>
     </div>
-    <div class="project_info">
-        <div class="project_info_text">
-            <h4>Grafica</h4>
-            <p>Ideazione e realizzazione di un poster promozionale.</p>
+    <div class="article">
+        <div class="title" id="about_chisono">
+            <h3>CHI SONO</h3>
+            <img alt="Web Tailoring" src="<?php echo Yii::app()->request->baseUrl?>/images/tailoring_high.jpg">
+            <div class="clear"></div>
         </div>
-        <img alt="Web Tailoring" src="images/ago.jpg">
-        <ul class="project_info_text2">
-            <li><b>Cliente:</b> Personale</li>
-            <li><b>Anno:</b> 2009</li>
-            <li><b>Strumenti:</b> photoshop, illustrator</li>
-            <li class="btn_project"></li>
-        </ul>
-        <div class="clear"></div>
+        <div class="about_info">
+            <p class="about_info_par01">Sono Danilo Giagnoli, un grafico web designer con più di 8 anni di esperienza.</p>
+            <p class="about_info_par02"> Mi occupo della progettazione e della realizzazione di siti web curando i progetti in ogni minimo dettaglio:
+                architettura informativa, wireframe, user experience, concept grafico e implementazione (xhtml/css).<br>
+                <br>
+                Tra le varie esperienze maturate, in agenzia o come freelance, ho avuto la possibilità di confrontarmi con molteplici problematiche
+                e tipologie di lavori, che mi hanno permesso di spaziare ed esplorare diversi stili, rendendomi un creativo dinamico e versatile
+                in grado di realizzare un "vestito fatto su misura".<br>
+                <br>
+                Offro servizi legati al web (siti internet, personalizzazione cms, conversioni da psd a html, banner pubblicitari) e alla grafica (loghi, immagine coordinata, brochure, cataloghi, flyer, inviti, illustrazioni).<br>
+                <br>
+                Se siete interessati ad avere una mia consulenza o collaborazione, non esitate a contattarmi. </p>
+            <div class="about_info_cvMail">
+                <p class="about_info_cv"><img alt="cv icon" src="images/cv_image.jpg"><a target="_blank" href="cv/cv_danilogiagnoli.pdf">SCARICA IL C.V.</a></p>
+                <p class="about_info_mail">info@danilogiagnoli.com</p>
+            </div>
+        </div>
+        <div class="project_info">
+            <div class="project_info_text">
+                <h4>Grafica</h4>
+                <p>Ideazione e realizzazione di un poster promozionale.</p>
+            </div>
+            <img alt="Web Tailoring" src="images/ago.jpg">
+            <ul class="project_info_text2">
+                <li><b>Cliente:</b> Personale</li>
+                <li><b>Anno:</b> 2009</li>
+                <li><b>Strumenti:</b> photoshop, illustrator</li>
+                <li class="btn_project"></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
     </div>
 </div>
